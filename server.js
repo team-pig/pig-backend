86cedec830e1496e8fca0e41b72ccc679558efa2
donghyun1500 <kdh9731@gmail.com>
2021-07-27 03:04:57 +0900

    users(login,register)

--- server.js
+++ server.js
@@ -1,37 +1,36 @@
 const express = require('express');
+require('express-async-errors');
 const app = express();
 const port = 3000;
+const dotenv = require('dotenv')
+dotenv.config();
+
 
 // 몽고db 붕어빵 틀
 const connect = require('./schemas/index');
-connect();
+connect()
 
 // 바디,json,media 데이터
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static('public'));
 
 // 라우터
-const routers = require("./routers");
-app.use("/api", [routers]);
+const userRouters = require("./routers/user.js");
+app.use("/api", [userRouters]);
 
 app.use((req, res, next) => {
-    console.log(req);
-    next();
+    res.sendStatus(404);
 });
 
-// 템플릿 엔진
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+// error handling
+app.use((error, req, res, next) => {
+    console.error(error);
+    res.sendStatus(500);
+});
 
-
-// 각종 url
-app.get('/', (req, res) => {
-    res.render('index.ejs');
-})
 
 
 app.listen(port, () => {
     console.log(`listening at http://localhost:${port}`);
-  })
-  
+})
