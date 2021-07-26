bb8611f47fc2fcb32e9142784ad4938793d400a9
awrde <awrde26@gmail.com>
2021-07-26 18:06:04 +0900

    Add basic skeleton API

--- routers/room.js
+++ routers/room.js
@@ -1,7 +1,38 @@
 import express from 'express'
-import { Room } from '../schemas/room.js'
+import Room from '../schemas/room.js'
 
 const router = express.Router()
 
+router.get('/rooms', auth, async (req, res) => {
+    
+  })
+  
+router.get('/room/:roomId/main', auth, async (req, res) => {
+
+})
+
+router.get('/room/:roomId/page', auth, async (req, res) => {
+
+})
+
+router.get('/room/:roomId/board', auth, async (req, res) => {
+
+})
+
+router.get('/room/:roomId/timeline', auth, async (req, res) => {
+
+})
+
+router.post('/room', auth, async (req, res) => {
+
+})
+
+router.put('/room', auth, async (req, res) => {
+    
+})
+
+router.delete('/room', auth, async (req, res) => {
+    
+})
 
 export default router
