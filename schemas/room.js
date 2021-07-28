85f0f69b8d21f2cde3de9b527aa6d47552179228
awrde <awrde26@gmail.com>
2021-07-28 23:08:06 +0900

    Schema cleanup

--- schemas/room.js
+++ schemas/room.js
@@ -1,30 +1,24 @@
 const mongoose = require('mongoose')
 // const Timeline = require('./timeline')
 const Document = require('./document')
-const { Schema } = mongoose;
+const { Schema } = mongoose
 
-const roomSchema = new mongoose.Schema({
-  roomName: String,
-  roomImage: String,
-  // master: { type: Schema.Types.ObjectId, ref: 'User' },
-  master: String,
-  inviteCode: String,
-  // inviteCode: mongoose.Types.ObjectId(),
-  subtitle: String,
-  tag: [String],
-  // members: [{
-  //   type: Schema.ObjectId,
-  //   ref: 'User'
-  // }],
-  members: [String],
-  // timeline: [Timeline.schema],
-  document: [Document.schema],
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  } 
-},
+const roomSchema = new mongoose.Schema(
   {
-    versionKey: false 
-})
-module.exports = mongoose.model('Room', roomSchema);
+    roomName: String,
+    roomImage: String,
+    master: String,
+    inviteCode: String,
+    subtitle: String,
+    tag: [String],
+    members: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    versionKey: false,
+  }
+)
+module.exports = mongoose.model('Room', roomSchema)
