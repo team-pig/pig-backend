fc4e10750bb45b51983a68078699e2a790fe2563
awrde <awrde26@gmail.com>
2021-08-05 15:33:50 +0900

    expiresIn 2d로 변경

--- routers/user.js
+++ routers/user.js
@@ -10,7 +10,7 @@ const router = express.Router();
 // let refreshTokens = []
 
 function createJwtToken(id) {
-    return jwt.sign({ id }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10m' });
+    return jwt.sign({ id }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2d' });
 }
 
 const registerValidator = Joi.object({
