c7de1dbeed2e34f64b20a658ad653523ed308280
awrde <awrde26@gmail.com>
2021-08-14 01:11:27 +0900

    Update memberStatus 스키마 desc 디폴트값 설정

--- schemas/memberStatus.js
+++ schemas/memberStatus.js
@@ -3,7 +3,7 @@ const memberStatusSchema = new mongoose.Schema({
   roomId : String,
   userId: String,
   nickname: String,
-  desc: String,
+  desc: {type:String, default: null},
   tags: [String],
   checked: Number,
   notChecked: Number
