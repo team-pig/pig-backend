c4ee4d77818091442dcbddc244b4c9764452e915
awrde <awrde26@gmail.com>
2021-08-20 22:04:55 +0900

    Add 방만들기 테스트 케이스를 위한 clearData 추가

--- tests/clearData.js
+++ tests/clearData.js
@@ -11,8 +11,14 @@ const CLEAR_DATA = {
   registerEmail: "test@naver.com",
   registerNickname: "실험맨",
   registerPassword: "test11",
-  registerConfirmPassword: "test11"
+  registerConfirmPassword: "test11",
 
+  room:{
+  roomName: "피카츄 가랏 백만볼트",
+	roomImage: "cat.img",
+	subtitle: "부제목",
+  tag: "이상해씨, 꼬부기",
+	desc: "나는 웅이라고 해! "}
 };
 
 module.exports = CLEAR_DATA;
