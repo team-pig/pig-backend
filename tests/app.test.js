1698b947f028de845a75dd1d8dcd1926f08fcf3b
awrde <awrde26@gmail.com>
2021-08-20 23:20:19 +0900

    Add 방 검색하기 성공 테스트 케이스 추가
    ?roomName=피카츄 처럼 한글을 적으면 TypeError [ERR_UNESCAPED_CHARACTERS]: Request path contains unescaped characters
    뜨며 테스트 케이스가 실패한다.

--- tests/app.test.js
+++ tests/app.test.js
@@ -124,6 +124,14 @@ describe('즐겨찾기 안된 방 불러오기 성공', () => {
   })
 })
 
+describe('방 검색하기 성공', () => {
+  it('GET room search success', async () => {
+    const res = await request.get(`/rooms/search?roomName=hangle 1`).auth(access, {type: 'bearer'})
+    expect(res.statusCode).toBe(200)
+    expect(res.body.room).toBeTruthy()
+  })
+})
+
 describe('방 삭제 성공', () => {
   it('delete room success', async () => {
     const res = await request.delete('/room').auth(access, {type: 'bearer'}).send({
