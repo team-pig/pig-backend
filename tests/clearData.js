1464c782cccbc20b5b17737516398314f46f59c8
awrde <awrde26@gmail.com>
2021-08-21 10:58:10 +0900

    Add myprofile 테스트 케이스를 위한 clearData 추가

--- tests/clearData.js
+++ tests/clearData.js
@@ -18,7 +18,12 @@ const CLEAR_DATA = {
 	roomImage: "cat.img",
 	subtitle: "부제목",
   tag: "이상해씨, 꼬부기",
-	desc: "나는 웅이라고 해! "}
+	desc: "나는 웅이라고 해! "},
+  
+  myprofile:{
+  desc: "우리 조에서 귀여움을 맡아요 냄새도 잘 맡아요", 
+  tags: ['귀염둥이', '막내', 'INTP']
+  }
 };
 
 module.exports = CLEAR_DATA;
