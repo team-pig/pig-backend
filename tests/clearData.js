04ae04b28efe127c8ef6f93d0897472ebbec4529
awrde <awrde26@gmail.com>
2021-08-21 12:00:15 +0900

    Add clearData room2 추가

--- tests/clearData.js
+++ tests/clearData.js
@@ -19,7 +19,16 @@ const CLEAR_DATA = {
 	subtitle: "부제목",
   tag: "이상해씨, 꼬부기",
 	desc: "나는 웅이라고 해! "},
+
+  room2:{
+    roomName: "청산에 살어리랏다",
+    roomImage: "mountain.img",
+    subtitle: "청산별곡",
+    tag: ["청산, 별곡"],
+    desc: " 설명은 생략하오 ",
+    endDate: Date.now()+ 10000},
   
+
   myprofile:{
   desc: "우리 조에서 귀여움을 맡아요 냄새도 잘 맡아요", 
   tags: ['귀염둥이', '막내', 'INTP']
