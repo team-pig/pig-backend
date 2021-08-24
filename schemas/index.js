eca2b7d04e51e2b4044279be9c759fd1ee985f57
awrde <awrde26@gmail.com>
2021-08-24 15:35:15 +0900

    Update 로컬 개발 환경으로 변경

--- schemas/index.js
+++ schemas/index.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose')
-const dotenv = require('dotenv');
-dotenv.config();
 
 const connect = () => {
   mongoose
-    .connect(`mongodb://${process.env.MONGODB_ID_PASSWORD}@localhost:27017/admin`, {
+    .connect('mongodb://test:test@localhost:27017/admin', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
