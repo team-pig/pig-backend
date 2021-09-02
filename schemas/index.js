2f885c2e74c90f0e0184d7ecd397726945b67f21
onanalee <69330162+onanalee@users.noreply.github.com>
2021-09-02 14:04:15 +0900

    code cleanup

--- schemas/index.js
+++ schemas/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connect = () => {
   mongoose
-    .connect('mongodb://test:test@localhost:27017/admin', {
+    .connect('mongodb://localhost:27017/admin', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
