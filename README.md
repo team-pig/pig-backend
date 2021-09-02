49ed53ef2d39bb8862587faafa9e178f0ae1ce28
donghyun1500 <kdh9731@gmail.com>
2021-09-02 22:42:21 +0900

    update README.md

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 <br>
 
 # **향해99 2기 | 실전 프로젝트 [협업돼지]**
+
 <div align="center">
   <a href="https://teampig.co.kr"><img src="https://i.ibb.co/gSrjxmS/image.jpg"/> 클릭 시 실제 서비스 페이지로 이동합니다.</a>
 </div>
@@ -13,13 +14,15 @@
 <br>
 <br>
 
-## 목차 | Contents
+## **목차 | Contents**
+
 1. [협업돼지 서비스 소개](#협업돼지-서비스-소개)
 2. [프로젝트 공동 행동강령](#프로젝트-공동-행동강령)
-3. 팀원소개
-4. 아키텍처
-5. ERD
-6. 
+3. [팀원소개](#팀원소개)
+4. [Git 운영 전략](#Git-운영-전략)
+5. [주요 라이브러리](#주요-라이브러리)
+6. [Architecture](#Architecture)
+7. [ERD](#ERD)
 
 <br>
 
@@ -28,45 +31,60 @@
 ## **🐷 협업돼지 서비스 소개**
 
 - **문서 작성 따로, 일정관리 따로, 메신저 따로... 따로.따로.따로😣<br>협업에 불편함을 느껴본 적 있으시죠?<br>**
-- **저희는 이런 불편을 없애기 위해 하나의 서비스에서 협업에 관련된 모든 것을 간편하게 진행할 수 있도록 하자는 공동의 목표로 탄생했습니다.**
-
-
-<br>
-<br>
-
-
-##  **🐱‍🏍 프로젝트 공동 행동강령**
-
-서비스의 모든 것은 최대한 유저의 입장을 생각하여 설계한다.<br>
-유저에게 불편을 줄만한 요소를 최대한 제거한다.<br>
-사소한 기능일지라도 유저가 편리하다고 느낀다면 추가한다.
-
-공동의 목표인 완성도 높은 서비스를 위해 서로 배려하고 최대한 밝은 분위기로 소통한다.
-
-새로운 기술이 좋다고 무조건 쓰기 보다 기술을 도입할 때는 항상 이유를 생각하며 사용한다.
-
-완벽을 추구하지 않고 현재 상황에서 어떻게든 서비스가 돌아가게 하도록 한다. 
-
+- **저희는 이런 불편을 없애기 위해 하나의 서비스에서 협업에 관련된 모든 것을 한번에 진행할 수 있도록 하자는 공동의 목표로 탄생했습니다.**
 
 <br>
+<br>
+
+## **🐱‍🏍 프로젝트 공동 행동강령**
+
+✔ 서비스의 모든 것은 최대한 유저의 입장을 생각하여 설계한다.<br>
+✔ 유저에게 불편을 줄만한 요소를 최대한 제거한다.<br>
+✔ 사소한 기능일지라도 유저가 편리하다고 느낀다면 추가한다.<br>
+✔ 공동의 목표인 완성도 높은 서비스를 위해 서로 배려하고 최대한 밝은 분위기로 소통한다.<br>
+✔ 새로운 기술이 좋다고 무조건 쓰기 보다 기술을 도입할 때는 항상 이유를 생각하며 사용한다.<br>
+
+<br>
+
+## **🏊 팀원소개**
+
+#### [팀노션페이지](https://www.notion.so/b0bdcefd9ab440afa5bc8565e45a71d8)
+
+#### BackEnd (Node.js) - [Backend Github](https://github.com/team-pig/pig-backend)
+
+- 이현수(팀장) (https://github.com/onanalee)
+- 명재국 (https://github.com/awrde)
+- 김동현 (https://github.com/donghyun1500)
+
+#### Frontend (React) - [Frontend Github](https://github.com/team-pig/pig-frontend)
+
+- 예상기 (https://github.com/with-key)
+- 안지현 (https://github.com/jihyunan-dev)
+- 김아영 (https://github.com/slamdunk11)
+
+#### Design (UI/UX) - [Figma WireFrame](https://www.figma.com/file/0C63JnrDTaEF2GHQEF2qKx/TeamPig?node-id=0%3A1)
+
+- Anna
+- 정서윤
+
 <br>
 
 <!-- <!--  -->
-## 🏄‍♀️ **Git 운영 전략**
-![](https://i.ibb.co/QnpSrt4/git.png)
 
+## **🏄‍♀️ Git 운영 전략**
+
+![](https://i.ibb.co/QnpSrt4/git.png)
 
 ```
 main  : 테스트 서버에서 모든 테스트를 마친 성공적으로 작동되는 경우 실제 운영중인 본 서버로 바로 적용되는 브랜치
-develop : 본 서버에 적용 전 테스트를 위한 브랜치 
+develop : 본 서버에 적용 전 테스트를 위한 브랜치
 Room, User, Document...기타 등등 : develop에서 파생되어 각자 기능 별로 작업 중인 브랜치
 ```
 
 <!-- <!-- <br> -->
 <br>
 
-
-## 주요 라이브러리 | Main Library
+## **주요 라이브러리 | Main Library**
 
 - **Node.js**
 - **Express**
@@ -76,6 +94,19 @@ Room, User, Document...기타 등등 : develop에서 파생되어 각자 기능 
 
 <details>
 <summary> jsonwebtoken </summary>
+유저 회원가입 이후 로그인시 사용되는 토큰
+<br>
+</details>
+
+<details>
+<summary> bcrypt </summary>
+회원가입시 패스워드를 암호화하는 모듈
+<br>
+</details>
+
+<details>
+<summary> joi </summary>
+회원가입시 User schema의 유효성을 검사하는 모듈 
 <br>
 </details>
 
@@ -102,32 +133,30 @@ Room, User, Document...기타 등등 : develop에서 파생되어 각자 기능 
 <summary> dotenv </summary>
 MongoDB Id, Password 정보, gmail 계정 정보, access token 및 refresh token 시크릿 키 정보 관리
 <br>
-</details> 
+</details>
+
+<details>
+<summary> cors </summary>
+특정 도메인 요청 활성화
+<br>
+</details>
 
 <details>
 <summary> lodash </summary>
 쉬운 deep copy를 위한 사용
 <br>
-</details> 
+</details>
 
 <br>
 <br>
 
+## **📊 Architecture**
 
-click! ▶️ []()
-
-
-## 📊 **Architecture**
-
-![](https://i.ibb.co/72qGwDZ/Frame-761.png)
-
+![](https://i.ibb.co/M1G0Q7R/Group-242.png)
 
 <br>
 <br>
 
+## **🕸 ERD**
 
-## 🕸 **ERD**
 ![mysql edr](https://i.ibb.co/LgFxJx7/my-new-app-Dra-1.png)
-
-
-
--- routers/user.js
+++ routers/user.js
@@ -16,8 +16,8 @@ const Room = require('../schemas/room.js')
 const BucketOrder = require('../schemas/bucketOrder')
 const Buckets = require('../schemas/bucket')
 const Documents = require('../schemas/document');
-const deleteAll = require('../middlewares/deleting')
-const MemberStatus = require('../schemas/memberStatus.js')
+const deleteAll = require('../middlewares/deleting');
+const MemberStatus = require('../schemas/memberStatus.js');
 const Cards = require('../schemas/card');
 const Todos = require('../schemas/todo');
 // let refreshTokens = []
@@ -27,21 +27,18 @@ const Todos = require('../schemas/todo');
 router.post('/resetPassword/sendEmail', async (req, res, next) => {
 
   try {
-    const { email } = req.body
-    const findEmail = await User.findOne({ email: email }, { email: true })
+    const { email } = req.body;
+    const findEmail = await User.findOne({ email: email }, { email: true });
     const userId = findEmail._id
     try {
-      // if (findEmail.email != email) {
-      //     return res.status(400).json({ errorMessage: '협업돼지에 등록되지 않은 이메일입니다.' })
-      // }
       if (findEmail.email == email) {
-        const token = v4()
+        const token = v4();
         const data = {
           token,
           userId: userId,
           createdAt: Date.now(),
-        }
-        Auth.create(data)
+        };
+        Auth.create(data);
 
         transport
           .sendMail({
@@ -61,72 +58,67 @@ router.post('/resetPassword/sendEmail', async (req, res, next) => {
           })
           .then((send) => res.json(send))
           .catch((err) => next(err))
-      }
+      };
     } catch (error) {
-      console.log({ errorMessage: '인증코드 발급에 실패했습니다.' })
-      res.status(500).json({ errorMessage: '인증코드 발급에 실패했습니다. 관리자에게 문의하세요.' })
-    }
+      console.log({ errorMessage: '인증코드 발급에 실패했습니다.' });
+      res.status(500).json({ errorMessage: '인증코드 발급에 실패했습니다. 관리자에게 문의하세요.' });
+    };
   } catch (err) {
     return res.status(400).json({ errorMessage: '협업돼지에 등록되지 않은 이메일입니다.' })
-  }
-})
+  };
+});
 
 router.get('/resetPassword/:token', async (req, res) => {
   try {
-    const findAuth = await Auth.findOne({ token: token })
+    const findAuth = await Auth.findOne({ token: token });
     if (Date.now() - findAuth.createdAt > 180000) {
       return res.status(400).json({ errorMessage: '인증코드가 만료되었습니다.' })
-    }
-    res.json({ message: '정상적으로 이동하였습니다.' })
+    };
+    res.json({ message: '정상적으로 이동하였습니다.' });
   } catch (error) {
-    res.status(400).json({ errorMessage: '이미 사용된 인증코드 또는 잘못된 인증코드입니다.' })
-  }
-})
+    res.status(400).json({ errorMessage: '이미 사용된 인증코드 또는 잘못된 인증코드입니다.' });
+  };
+});
 
 router.post('/resetPassword/:token', async (req, res) => {
   // 입력받은 token 값이 Auth 테이블에 존재하며 아직 유효한지 확인
   try {
-    const token = req.params.token
-    const { password, confirmPassword } = req.body
-    const findAuth = await Auth.findOne({ token: token })
+    const token = req.params.token;
+    const { password, confirmPassword } = req.body;
+    const findAuth = await Auth.findOne({ token: token });
     // 본 서버 적용   인증코드는 3분의 유효기간(180000ms)
     if (Date.now() - findAuth.createdAt > 180000) {
-      return res.status(400).json({ message: '인증코드가 만료되었습니다. ' })
-    }
+      return res.status(400).json({ message: '인증코드가 만료되었습니다. ' });
+    };
     if (password != confirmPassword) {
-      res.status(400).json({ errorMessage: '패스워드가 일치하지 않습니다.' })
-    }
-    const userId = findAuth.userId
-    const salt = await bcrypt.genSalt()
-    const hashed = await bcrypt.hash(password, salt)
-    const findUser = await User.findOneAndUpdate({ _id: userId }, { $set: { password: hashed } })
-    await Auth.findOneAndDelete({userId: userId})
+      res.status(400).json({ errorMessage: '패스워드가 일치하지 않습니다.' });
+    };
+    const userId = findAuth.userId;
+    const salt = await bcrypt.genSalt();
+    const hashed = await bcrypt.hash(password, salt);
+    const findUser = await User.findOneAndUpdate({ _id: userId }, { $set: { password: hashed } });
+    await Auth.findOneAndDelete({userId: userId});
     res.status(201).json({
       message: '비밀번호 재설정 성공',
       email: findUser.email,
       nickname: findUser.nickname,
-    })
+    });
   } catch (error) {
-    res.status(400).json({ errorMessage: '이미 사용된 인증코드거나 잘못된 인증코드입니다.' })
-  }
+    res.status(400).json({ errorMessage: '이미 사용된 인증코드거나 잘못된 인증코드입니다.' });
+  };
 
-})
-
-// function createJwtToken(id, color, avatar) {
-//     return jwt.sign({ id, color, avatar}, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '7d' });
-// }
-
+});
 
 const registerValidator = Joi.object({
     email: Joi.string().email().required(),
-    nickname: Joi.string().min(3).max(20).required(),
+    nickname: Joi.string().min(2).max(20).required(),
     password: Joi.string()
         .pattern(new RegExp('^(?=.*[a-zA-Z])(?=.*[0-9]).{5,30}$')) //5자 ~ 30자, 영어와 숫자만 허용
         .required(),
     confirmPassword: Joi.ref('password'),
     color: Joi.string().allow(''),
     avatar: Joi.string().allow('')
-}).with('password', 'confirmPassword')
+}).with('password', 'confirmPassword');
 
 
 router.post('/register', async (req, res, next) => {
@@ -138,16 +130,16 @@ router.post('/register', async (req, res, next) => {
                 errorMessage: "패스워드가 일치하지 않습니다.",
             });
             return;
-        }
+        };
 
-        // 닉네임 3글자 미만은 회원가입 불가.
+        // 닉네임 2글자 미만은 회원가입 불가.
         const nickName = await User.findOne({ nickname })
-        if (nickName != null && nickName.length < 3) {
+        if (nickName != null && nickName.length < 2) {
             res.status(400).send({
                 errorMessage: '닉네임에 적합하지 않습니다.'
             });
             return;
-        }
+        };
 
         // email and nickname이 동일한게 이미 있는지 확인하기 위해 가져온다.
         const existsEmail = await User.findOne({ email })
@@ -157,13 +149,13 @@ router.post('/register', async (req, res, next) => {
                 errorMessage: "이메일이 이미 사용중입니다.",
             });
             return;
-        }
+        };
         if (existsNickname) {
             res.status(400).send({
                 errorMessage: "닉네임이 이미 사용중입니다."
             });
             return;
-        }
+        };
         const salt = await bcrypt.genSalt();
         const hashed = await bcrypt.hash(password, salt);
 
@@ -176,7 +168,7 @@ router.post('/register', async (req, res, next) => {
         });
         await Tutorial.create({
           userId: user._id
-        })
+        });
         res.status(201).json({
             ok: true,
             message: '회원가입 성공',
@@ -188,27 +180,24 @@ router.post('/register', async (req, res, next) => {
 
     } catch (error) {
         next(error);
-    }
+    };
 });
 
 router.post('/login', async (req, res, next) => {
     try {
         const { email, password } = req.body;
-        const user = await User.findOne({ email })
+        const user = await User.findOne({ email });
         if (!user) {
             return res.status(401).json({ errorMessage: '이메일 또는 패스워드가 틀렸습니다.' });
-        }
+        };
 
         const isValidPassword = await bcrypt.compare(password, user.password);
         if (!isValidPassword) {
             return res.status(401).json({ errorMessage: '이메일 또는 패스워드가 틀렸습니다.' });
-        }
+        };
         let accessToken = jwt.sign({ id: user.id, color: user.color, avatar: user.avatar }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30m' });
 
-        let refreshToken = jwt.sign({ id: user.id } , process.env.REFRESH_TOKEN_SECRET, {expiresIn: '7d'})
-
-        // refreshTokens.push(refreshToken);
-
+        let refreshToken = jwt.sign({ id: user.id } , process.env.REFRESH_TOKEN_SECRET, {expiresIn: '7d'});
         res.status(200).json({
             ok: true,
             message: '로그인 성공',
@@ -219,9 +208,9 @@ router.post('/login', async (req, res, next) => {
         res.status(400).send({
             ok: false,
             errorMessage: '서버 실패: 로그인 실패',
-        })
+        });
         next(error);
-    }
+    };
 });
 
 router.post('/push/tutorial', async (req, res) => {
@@ -229,52 +218,51 @@ router.post('/push/tutorial', async (req, res) => {
     const findUsers = await User.find({})
     for (let i = 0; i < findUsers.length; i++) {
       var userId = findUsers[i]._id
-      await Tutorial.create({ userId })
-    }
-    console.log(findUsers)
-    res.send('hi')
+      await Tutorial.create({ userId });
+    };
+    res.send('hi');
   } catch (err) {
-    res.status(400).send({ errorMessage: 'bye' })
-  }
-})
+    res.status(400).send({ errorMessage: 'bye' });
+  };
+});
 
 router.patch('/tutorial', authMiddleware, async (req, res) => {
   try {
-    const userId = res.locals.user._id
-    const { roomlist, main, document, board, calender, modal } = req.body.tutorial
-    await Tutorial.findOneAndUpdate({ userId }, { $set: { roomlist, main, document, board, calender, modal } }).lean()
-    const tutorial = await Tutorial.findOne({ userId }).lean()
-    res.json({ message: '성공', tutorial })
+    const userId = res.locals.user._id;
+    const { roomlist, main, document, board, calender, modal } = req.body.tutorial;
+    await Tutorial.findOneAndUpdate({ userId }, { $set: { roomlist, main, document, board, calender, modal } }).lean();
+    const tutorial = await Tutorial.findOne({ userId }).lean();
+    res.json({ message: '성공', tutorial });
   } catch (err) {
-    return res.status(400).json({ errorMessage: '에러남' })
-  }
-})
+    return res.status(400).json({ errorMessage: '에러남' });
+  };
+});
 
 router.get('/token', authMiddleware, async (req, res, next) => {
   try {
-    const userId = res.locals.user._id
-    const tutorial = await Tutorial.findOne({ userId })
+    const userId = res.locals.user._id;
+    const tutorial = await Tutorial.findOne({ userId });
     res.status(200).send({
       ok: true,
       message: '토큰 인증 성공',
       user: res.locals.user,
       tutorial: tutorial,
-    })
+    });
   } catch (err) {
     res.status(400).send({
       ok: false,
       errorMessage: '토큰 인증 실패',
-    })
-    next(err)
-  }
-})
+    });
+    next(err);
+  };
+});
 
 
 router.post('/token', (req, res) => {
     const refreshToken = req.body.refreshToken;
     if (refreshToken == null) {
         return res.status(401).json({ errorMessage: '리프레쉬 토큰이 없습니다.' })
-    }
+    };
 
     jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
         if (!err) {
@@ -286,23 +274,23 @@ router.post('/token', (req, res) => {
             });
         } else {
             return res.status(403).json({ errorMessage: '리프레시 토큰 유효하지 않습니다.' })
-        }
-    })
+        };
+    });
 });
 // 회원 탈퇴 시 혼자 있던 room 찾아서 하위 항목들 다 삭제예정 현재는 임시 미완성 API
 router.delete('/userInfo', async (req, res) => {
     try {
-        const email = req.body.email
+        const email = req.body.email;
         // await findUser.delete({})
-        const remove = await User.findOneAndRemove({ email: email })
+        const remove = await User.findOneAndRemove({ email: email });
         if (!remove) {
             return res.status(400).json({ errorMessage: '이메일이 잘못되었습니다.' })
-        }
+        };
         res.json({ message: '회원탈퇴 성공' })
     } catch (err) {
         res.status(500).json({ errorMessage: '회원탈퇴 실패' })
-    }
-})
+    };
+});
 
 module.exports = router;
 
